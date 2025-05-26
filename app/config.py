efc1c903955bb98cda9fefe6023a61c580fbb371
Charlie Nicholson <75419961+charlienicholson@users.noreply.github.com>
2025-05-27 00:47:46 +0100

    Update config.py
    added CSRF enable

--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get("SECRET_KEY") or "CHANGE_ME_IN_PRODUCTION"
+    WTF_CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///secure_portal.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
